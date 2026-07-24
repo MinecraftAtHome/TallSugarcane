b6fef791a5bdc7f737e7a51fdd550e0d5e69cf73
Kludwisz <kludwisz@gmail.com>
2026-07-24 20:33:04 +0200

    finder sketch

--- java/SugarcanGeneration/src/main/java/finder/RavineSugarcaneFinder.java
+++ java/SugarcanGeneration/src/main/java/finder/RavineSugarcaneFinder.java
@@ -1,4 +1,17 @@
 package finder;
 
+import com.seedfinding.mccore.rand.ChunkRand;
+import feature.DirtPatchFeature;
+
 public class RavineSugarcaneFinder {
+    public static void main(String[] args) {
+        DirtPatchFeature dirt = new DirtPatchFeature();
+        ChunkRand rand = new ChunkRand();
+
+        // state-predict for ravine at 0,0
+        long spMax = (1L << 48) / 50;
+        for (long iseed = 0; iseed < spMax; iseed++) {
+
+        }
+    }
 }
